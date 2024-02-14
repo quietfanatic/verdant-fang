0c13b9f85a8d40260b70a96df60d57f6c8506555
quietfanatic <lewis@wall.org>
2024-02-13 21:25:45 -0800

    Adjust monster behavior

--- src/vf/world/monster.cpp
+++ src/vf/world/monster.cpp
@@ -141,134 +141,140 @@ Controls MonsterMind::Mind_think (Resident& s) {
         jump_dist = -jump_dist;
     }
 
-    switch (self.alert_phase) {
-        case 0: {
-            if (length(dist) < sight_range) {
-                self.alert_phase += 1;
-            }
-            else break;
-            [[fallthrough]];
-        }
-        case 1: {
-            if (self.alert_timer >= alert_sequence[0]) {
-                self.alert_phase += 1;
-                self.alert_timer = 0;
-            }
-            else {
-                self.alert_timer += 1;
-                 // Do nothing, haven't reacted yet
-                break;
-            }
-            [[fallthrough]];
-        }
-        case 2: {
-            if (self.alert_timer == 0) {
-                 // Alert other monsters
-                for (auto& r : self.room->residents) {
-                    if (!(r.types & Types::Monster)) continue;
-                    auto& fren = static_cast<Monster&>(r);
-                    if (fren.state == WS::Dead) continue;
-                    if (fren.alert_phase == 0) fren.alert_phase = 1;
-                }
-            }
-            if (self.alert_timer >= alert_sequence[1]) {
-                self.alert_phase += 1;
-                self.alert_timer = 0;
-            }
-            else {
-                self.alert_timer += 1;
-                 // Look at target
-                if (dist < 0) r[backward] = 1;
-                break;
-            }
-            [[fallthrough]];
-        }
-        case 3: {
-            if (defined(hiding_spot)) switch (self.hide_phase) {
-                case 0: {
-                    if (self.pos.x < hiding_spot + 24) {
-                        r[Control::Right] = 1;
-                        return r;
-                    }
-                    else self.hide_phase = 1;
-                    [[fallthrough]];
-                }
-                case 1: {
-                    if (self.pos.x > hiding_spot) {
-                        r[Control::Left] = 1;
-                        return r;
-                    }
-                    else self.hide_phase = 2;
-                    [[fallthrough]];
-                }
-                case 2: {
-                    if (self.left) {
-                        r[Control::Right] = 1;
-                        return r;
-                    }
-                    else if (dist > 48) {
-                        self.hide_phase = 3;
-                    }
-                    else {
-                        self.invincible = true;
-                        return r;
-                    }
-                    [[fallthrough]];
-                }
-                case 3: {
-                    if (self.pos.x > hiding_spot + 20) {
-                        self.hide_phase = 4;
-                    }
-                    [[fallthrough]];
-                }
-                case 4: break;
-                default: never();
-            }
-            if (dist < 0) {
-                r[backward] = 1;
-            }
-            else if (attack_dist < attack_range) {
-                if (
-                     // Don't attack too early when jumping unless the player is
-                     // above.
-                    (self.vel.y < 1 || target->pos.y > self.pos.y + 8) &&
-                     // Don't hold preattack pose
-                    (self.state != WS::Attack || self.anim_phase >= 3)
-                ) {
-                    r[Control::Attack] = 1;
-                }
-            }
-            else if (jump_dist < jump_range) {
-                r[forward] = 1;
-                r[Control::Jump] = 1;
-            }
-            else r[forward] = 1;
-             // Wait for or jump over other monsters
-            for (auto& other : self.room->residents) {
-                if (&other == &s || !(other.types & Types::Monster)) continue;
-                auto& fren = static_cast<Monster&>(other);
-                if (fren.state == WS::Dead) continue; // :(
-                if (fren.hide_phase == 1 || fren.hide_phase == 2) continue;
-                auto dist = self.left_flip(fren.pos.x - self.pos.x);
-                if (dist > 0 && dist < social_distance) {
-                    if (jump_dist && fren.left != self.left) {
-                        r[Control::Jump] = 1;
-                    }
-                    else {
-                        r[Control::Jump] = 0;
-                        r[forward] = 0;
-                    }
-                    break;
-                }
-            }
-             // If we're behind scenery, keep going right no matter what
-            if (self.hide_phase == 2) {
-                r[Control::Right] = 1;
-                r[Control::Left] = 0;
-            }
-            break;
+    next_alert_phase:
+    if (self.alert_phase == 0) {
+        if (length(dist) < sight_range) {
+            self.alert_phase = 1;
+            goto next_alert_phase;
         }
     }
+    else if (self.alert_phase == 1) {
+        if (self.alert_timer >= alert_sequence[0]) {
+            self.alert_phase = 2;
+            self.alert_timer = 0;
+            goto next_alert_phase;
+        }
+        else {
+            self.alert_timer += 1;
+             // Do nothing, haven't reacted yet
+        }
+    }
+    else if (self.alert_phase == 2) {
+        if (self.alert_timer == 0) {
+             // Alert other monsters
+            for (auto& r : self.room->residents) {
+                if (!(r.types & Types::Monster)) continue;
+                auto& fren = static_cast<Monster&>(r);
+                if (fren.state == WS::Dead) continue;
+                if (fren.alert_phase == 0) fren.alert_phase = 1;
+            }
+        }
+        if (self.alert_timer >= alert_sequence[1]) {
+            self.alert_phase = 3;
+            self.alert_timer = 0;
+            goto next_alert_phase;
+        }
+        else {
+            self.alert_timer += 1;
+             // Look at target
+            if (dist < 0) r[backward] = 1;
+        }
+    }
+    else if (self.alert_phase == 3) {
+        if (defined(hiding_spot)) {
+            next_hide_phase:
+            if (self.hide_phase == 0) {
+                if (self.pos.x < hiding_spot + 24) {
+                    r[Control::Right] = 1;
+                    return r;
+                }
+                else {
+                    self.hide_phase = 1;
+                    goto next_hide_phase;
+                }
+            }
+            else if (self.hide_phase == 1) {
+                if (self.pos.x > hiding_spot) {
+                    r[Control::Left] = 1;
+                    self.invincible = true;
+                    return r;
+                }
+                else {
+                    self.hide_phase = 2;
+                    goto next_hide_phase;
+                }
+            }
+            else if (self.hide_phase == 2) {
+                if (self.left) {
+                    r[Control::Right] = 1;
+                }
+                else if (dist > 48) {
+                     // Don't come out until you're the only one left
+                    bool others = false;
+                    for (auto& r : self.room->residents) {
+                        if (!(r.types & Types::Monster)) continue;
+                        auto& m = static_cast<Monster&>(r);
+                        if (&m == &self || m.state == WS::Dead) continue;
+                        others = true; break;
+                    }
+                    if (!others) {
+                        self.hide_phase = 3;
+                        goto next_hide_phase;
+                    }
+                }
+                self.invincible = true;
+                return r;
+            }
+            else if (self.hide_phase == 3) {
+                if (self.pos.x > hiding_spot + 20) {
+                    self.hide_phase = 4;
+                }
+            }
+        }
+        if (dist < 0) {
+            r[backward] = 1;
+        }
+        else if (attack_dist < attack_range) {
+            if (
+                 // Don't attack too early when jumping unless the player is
+                 // above.
+                (self.vel.y < 1 || target->pos.y > self.pos.y + 8) &&
+                 // Don't hold preattack pose
+                (self.state != WS::Attack || self.anim_phase >= 3)
+            ) {
+                r[Control::Attack] = 1;
+            }
+        }
+        else if (jump_dist < jump_range) {
+            r[forward] = 1;
+            r[Control::Jump] = 1;
+        }
+        else r[forward] = 1;
+         // Wait for or jump over other monsters
+        for (auto& other : self.room->residents) {
+            if (&other == &s || !(other.types & Types::Monster)) continue;
+            auto& fren = static_cast<Monster&>(other);
+            if (fren.state == WS::Dead) continue; // :(
+            if (fren.hide_phase == 1 || fren.hide_phase == 2) continue;
+            auto dist = self.left_flip(fren.pos.x - self.pos.x);
+            if (dist > 0 && dist < social_distance) {
+                if (jump_dist && fren.left != self.left) {
+                    r[Control::Jump] = 1;
+                }
+                else {
+                    r[Control::Jump] = 0;
+                    r[forward] = 0;
+                }
+                break;
+            }
+        }
+         // If we're behind scenery, keep going right no matter what
+        if (self.hide_phase == 3) {
+            r[Control::Right] = 1;
+            r[Control::Left] = 0;
+        }
+    }
+    else never();
     return r;
 }
 
